7dc659f4a920615e934866cf93cc112099cd4020
Evgeniya Alekhnovich <evegniya.alehnovich@gmail.com>
2024-08-08 20:38:59 +0300

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import {Routes} from 'react-router-dom';
 import { Header } from './Header/Header';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        {/* <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} /> */}
-      </Routes>
     </>
   );
 }
